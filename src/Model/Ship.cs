f44c59fb6bc2440ad2569ea8b2506c4cf7c07980
Eva <100088776@students.swinburne.edu.my>
2019-10-09 20:33:53 +0800

    Adding Ship.cs

--- src/Model/Ship.cs
+++ src/Model/Ship.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualBasic;
 using System;
 using System.Collections;
 using System.Collections.Generic;
-// using System.Data;
 using System.Diagnostics;
 /// <summary>
 /// A Ship has all the details about itself. For example the shipname,
